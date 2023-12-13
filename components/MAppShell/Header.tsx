81a153ac22a1e0563aebb248fcd07751eb29092d
Vadim Rumyantsev <warum48@gmail.com>
2023-12-13 18:43:53 +0700

    hide settings

--- components/MAppShell/Header.tsx
+++ components/MAppShell/Header.tsx
@@ -5,27 +5,29 @@ import * as React from 'react';
 //import { DemoLogIn } from '../DemoLogin/DemoLogIn';
 import dynamic from 'next/dynamic';
 
-const DemoLogIn=  dynamic(() => import('../DemoLogin/DemoLogIn').then((mod) => mod.DemoLogIn) , {
-    ssr: false,
-  }) 
+const DemoLogIn = dynamic(() => import('../DemoLogin/DemoLogIn').then((mod) => mod.DemoLogIn), {
+  ssr: false,
+});
 
 type TProps = {
-    opened: boolean;
-    setOpened: React.Dispatch<React.SetStateAction<boolean>>;
-    asideOpen: boolean;
-    setAsideOpen: React.Dispatch<React.SetStateAction<boolean>>;
-}
+  opened: boolean;
+  setOpened: React.Dispatch<React.SetStateAction<boolean>>;
+  asideOpen: boolean;
+  setAsideOpen: React.Dispatch<React.SetStateAction<boolean>>;
+};
 import styles from './Header.module.css';
+import { ColorSchemeButton } from '../ColorSchemeButton/ColorSchemeButton';
 
 export const Header = ({ opened, setOpened, asideOpen, setAsideOpen }: TProps) => {
   const theme = useMantineTheme();
   const largeScreen = useMediaQuery('(min-width: 88em)');
   const midUpScreen = useMediaQuery('(min-width: 75em)');
+  const [showSettings, setShowSettings] = React.useState(false);
 
   return (
     <Box className={styles.container}>
       <Burger
-      hiddenFrom="lg"
+        hiddenFrom="lg"
         opened={opened}
         onClick={() => setOpened((o) => !o)}
         size="sm"
@@ -34,35 +36,28 @@ export const Header = ({ opened, setOpened, asideOpen, setAsideOpen }: TProps) =
         className={styles.burger}
       />
       <Box className={styles.logoContainer}>
-        <Image
-          className={styles.logo}
-          fit="contain"
-          src="/images/logo.svg"
-          height={40}
-        />
+        <Image className={styles.logo} fit="contain" src="/images/logo.svg" height={40} />
       </Box>
-     {/*} <Text className={styles.title}>
+      {/*} <Text className={styles.title}>
         <i>ЛИЧНЫЙ КАБИНЕТ ПОЛЬЗОВАТЕЛЯ</i>
   </Text> */}
-     {/* <Group className={styles.contactGroup}>
+      {/* <Group className={styles.contactGroup}>
         <Box className={styles.contactIcon}>
           <IconPhone size={40} color="#e64980" />
 </Box> */}
-        <Box className={styles.contactGroup}>
-          <Title order={4}>+7 (812) 424-64-74</Title>
-          <Text fw={500} fz="xs" ml={'3px'}>
-            Контактный центр 24 часа 
-          </Text>
-        </Box>
-    {/*}  </Group> */}
-      <DemoLogIn suppressHydrationWarning={true} />
-      <Box
-        className={styles.settingsIcon}
-        onClick={(e) => setAsideOpen(!asideOpen)}
-        m='xs'
-      >
-        <IconSettings />
+      <Box className={styles.contactGroup}>
+        <Title order={4}>+7 (812) 424-64-74</Title>
+        <Text fw={500} fz="xs" ml={'3px'}>
+          Контактный центр 24 часа
+        </Text>
       </Box>
+      {/*}  </Group> */}
+      <DemoLogIn suppressHydrationWarning={true} />
+      <ColorSchemeButton />
+        {showSettings && 
+      <Box className={styles.settingsIcon} onClick={(e) => setAsideOpen(!asideOpen)} m="xs">
+        <IconSettings />
+      </Box>}
     </Box>
   );
-}
+};
