09f0cd6157e0a9abf5fab8c2f994a7c1d71e6837
Vadim Rumyantsev <warum48@gmail.com>
2023-12-28 17:24:00 +0700

    demo build fix

--- app/registration/page.tsx
+++ app/registration/page.tsx
@@ -1,14 +1,10 @@
 'use client';
 import { CodeConfirm } from '@/components/_registration/CodeConfirm';
-import { RegForm } from '@/components/_registration/RegForm';
+//import { RegForm } from '@/components/_registration/RegForm';
 import { RegFormMax } from '@/components/_registration/RegFormMax';
 import { RegSuccess } from '@/components/_registration/RegSuccess';
 import * as React from 'react';
 
-//import { FastCommentBlock } from '../../components/FastComment/FastCommentBlock';
-//import { CodeConfirm } from './CodeConfirm';
-//import { RegForm } from './RegForm';
-//import { RegSuccess } from './RegSuccess';
 
 export type TRegStep = 'regForm' | 'confirmCode' | 'success';
 
